d54cec412ff448c1b576c5bfc57e29792e2be7e1
simoniz0r <bnu.fantasma@gmail.com>
2018-03-11 14:16:16 -0500

    Reorganize

--- scripts/builddeb2appimage-prerun.sh
+++ scripts/builddeb2appimage-prerun.sh
@@ -4,7 +4,7 @@ mkdir -p "$HOME"/.cache/deb2appimage/AppDir/usr/bin
 mkdir -p "$HOME"/.cache/deb2appimage/AppDir/usr/share/deb2appimage
 cp ~/github/deb2appimage/deb2appimage.sh ~/.cache/deb2appimage/AppDir/usr/bin/deb2appimage
 chmod a+x ~/.cache/deb2appimage/AppDir/usr/bin/deb2appimage
-cp ~/github/deb2appimage/spm.png ~/.cache/deb2appimage/AppDir/usr/share/deb2appimage/deb2appimage.png
+cp ~/github/deb2appimage/deb2appimage.png ~/.cache/deb2appimage/AppDir/usr/share/deb2appimage/deb2appimage.png
 
 cat > ~/.cache/deb2appimage/AppDir/usr/share/deb2appimage/deb2appimage.desktop << EOL
 [Desktop Entry]
